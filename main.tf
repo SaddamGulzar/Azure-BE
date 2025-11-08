3409590e98e4bdf6398f4d550f7a2d22490721a7
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 22:39:44 +0500

    Workflow Added

--- main.tf
+++ main.tf
@@ -2,97 +2,116 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
+      version = "~> 3.0"
     }
   }
+  required_version = ">= 1.5.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-# -----------------------------
-# Use existing Resource Group
-# -----------------------------
-data "azurerm_resource_group" "devops" {
-  name = "DevOps"
+# -------------------------
+# Resource Group
+# -------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "DevOps"
+  location = "East US"
 }
 
-# -----------------------------
-# Storage Account for Function
-# -----------------------------
+# -------------------------
+# Storage Account for Function App
+# -------------------------
 resource "azurerm_storage_account" "funcsa" {
   name                     = "azurebefuncsa"
-  resource_group_name      = data.azurerm_resource_group.devops.name
-  location                 = data.azurerm_resource_group.devops.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# -----------------------------
-# Cosmos DB (Table API)
-# -----------------------------
+# -------------------------
+# Cosmos DB Account
+# -------------------------
 resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "azure-be"
-  location            = data.azurerm_resource_group.devops.location
-  resource_group_name = data.azurerm_resource_group.devops.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
-
   consistency_policy {
     consistency_level = "Session"
   }
-
   geo_location {
-    location          = data.azurerm_resource_group.devops.location
+    location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
-
-  capabilities {
-    name = "EnableTable"
-  }
 }
 
-# Database Table
-resource "azurerm_cosmosdb_table" "table" {
-  name                = "counter"
-  resource_group_name = data.azurerm_resource_group.devops.name
-  account_name        = azurerm_cosmosdb_account.cosmos.name
-}
-
-# -----------------------------
-# Service Plan (Linux)
-# -----------------------------
+# -------------------------
+# App Service Plan
+# -------------------------
 resource "azurerm_service_plan" "function_plan" {
   name                = "azure-be-plan"
-  location            = data.azurerm_resource_group.devops.location
-  resource_group_name = data.azurerm_resource_group.devops.name
-  os_type             = "Linux"
-  sku_name            = "Y1"
-  worker_count        = 1
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
 
-# -----------------------------
-# Function App
-# -----------------------------
+# -------------------------
+# Linux Function App
+# -------------------------
 resource "azurerm_linux_function_app" "function_app" {
-  name                       = "azure-be"
-  location                   = data.azurerm_resource_group.devops.location
-  resource_group_name        = data.azurerm_resource_group.devops.name
-  service_plan_id            = azurerm_service_plan.function_plan.id
+  name                = "azure-be-func"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.funcsa.name
   storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
-  functions_extension_version = "~4"
-
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "AzureWebJobsStorage"      = azurerm_storage_account.funcsa.primary_connection_string
-    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_key
-    "TABLE_NAME"               = azurerm_cosmosdb_table.table.name
-  }
+  os_type                   = "Linux"
 
   site_config {
-    linux_fx_version = "Python|3.11"
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME   = "python"
+    COSMOS_TABLE_ENDPOINT      = azurerm_cosmosdb_account.cosmos.table_endpoint
+    COSMOS_TABLE_KEY           = azurerm_cosmosdb_account.cosmos.primary_key
+    TABLE_NAME                 = "counter"
+  }
+}
+
+# -------------------------
+# Initialize Counter Table with Initial Entity
+# -------------------------
+resource "null_resource" "create_initial_counter" {
+  depends_on = [azurerm_linux_function_app.function_app]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      az cosmosdb table create \
+        --account-name ${azurerm_cosmosdb_account.cosmos.name} \
+        --resource-group ${azurerm_resource_group.rg.name} \
+        --name counter \
+        --throughput 400
+
+      az cosmosdb table entity create \
+        --account-name ${azurerm_cosmosdb_account.cosmos.name} \
+        --resource-group ${azurerm_resource_group.rg.name} \
+        --table-name counter \
+        --partition-key "counter" \
+        --row-key "visitors" \
+        --properties '{"count":1}'
+    EOT
+    interpreter = ["bash", "-c"]
   }
 }
