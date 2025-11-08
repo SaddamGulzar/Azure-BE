9a1a359c2cc598a41ef7ceb4e6c73d2feefd54e6
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 22:19:32 +0500

    Workflow Added

--- main.tf
+++ main.tf
@@ -5,14 +5,47 @@ terraform {
       version = ">= 3.100.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
+  required_version = ">= 1.5.0"
 }
 
 # -----------------------------
-# Use existing Resource Group
+# Variables for Azure authentication
+# -----------------------------
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Service Principal Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Service Principal Client Secret"
+  sensitive   = true
+}
+
+# -----------------------------
+# Provider
+# -----------------------------
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+# -----------------------------
+# Existing Resource Group
 # -----------------------------
 data "azurerm_resource_group" "devops" {
   name = "DevOps"
@@ -30,7 +63,7 @@ resource "azurerm_storage_account" "funcsa" {
 }
 
 # -----------------------------
-# Cosmos DB (Table API)
+# Cosmos DB Account (Table API)
 # -----------------------------
 resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "azure-be"
@@ -53,7 +86,9 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   }
 }
 
-# Cosmos DB Table for Visitor Counter
+# -----------------------------
+# Cosmos DB Table
+# -----------------------------
 resource "azurerm_cosmosdb_table" "table" {
   name                = "counter"
   resource_group_name = data.azurerm_resource_group.devops.name
@@ -61,38 +96,45 @@ resource "azurerm_cosmosdb_table" "table" {
 }
 
 # -----------------------------
-# Linux Service Plan for Function App
+# Linux Function App Service Plan
 # -----------------------------
 resource "azurerm_service_plan" "function_plan" {
   name                = "azure-be-plan"
   location            = "Canada Central"
   resource_group_name = data.azurerm_resource_group.devops.name
   os_type             = "Linux"
-  sku_name            = "Y1" # Consumption Plan
+  sku_name            = "Y1"
   worker_count        = 1
 }
 
 # -----------------------------
-# Linux Function App
+# Function App
 # -----------------------------
 resource "azurerm_linux_function_app" "function_app" {
-  name                        = "azure-be"
-  location                    = "Canada Central"
-  resource_group_name         = data.azurerm_resource_group.devops.name
-  service_plan_id             = azurerm_service_plan.function_plan.id
-  storage_account_name        = azurerm_storage_account.funcsa.name
-  storage_account_access_key  = azurerm_storage_account.funcsa.primary_access_key
+  name                       = "azure-be"
+  location                   = "Canada Central"
+  resource_group_name        = data.azurerm_resource_group.devops.name
+  service_plan_id            = azurerm_service_plan.function_plan.id
+  storage_account_name       = azurerm_storage_account.funcsa.name
+  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
   functions_extension_version = "~4"
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "python"
     "AzureWebJobsStorage"      = azurerm_storage_account.funcsa.primary_connection_string
     "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_key
+    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_master_key
     "TABLE_NAME"               = azurerm_cosmosdb_table.table.name
   }
 
   site_config {
-    linux_fx_version = "PYTHON|3.11"
+    linux_fx_version = "Python|3.11"
   }
+}
+
+# -----------------------------
+# Optional: Output Function App URL
+# -----------------------------
+output "function_app_url" {
+  value = azurerm_linux_function_app.function_app.default_hostname
 }
