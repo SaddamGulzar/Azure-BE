7f50c30805173ed20a3a81c13f5ddb27b209f0b2
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 22:27:49 +0500

    Workflow Added

--- main.tf
+++ main.tf
@@ -5,13 +5,14 @@ terraform {
       version = ">= 3.100.0"
     }
   }
+  required_version = ">= 1.4.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-# Existing Resource Group
+# Use existing resource group
 data "azurerm_resource_group" "devops" {
   name = "DevOps"
 }
@@ -32,7 +33,7 @@ resource "azurerm_storage_account" "funcsa" {
 # -----------------------------
 resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "azure-be"
-  location            = "Canada Central"
+  location            = data.azurerm_resource_group.devops.location
   resource_group_name = data.azurerm_resource_group.devops.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
@@ -42,7 +43,7 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   }
 
   geo_location {
-    location          = "Canada Central"
+    location          = data.azurerm_resource_group.devops.location
     failover_priority = 0
   }
 
@@ -52,22 +53,22 @@ resource "azurerm_cosmosdb_account" "cosmos" {
 }
 
 # Cosmos DB Table
-resource "azurerm_cosmosdb_table" "table" {
-  name                = "counter"
+resource "azurerm_cosmosdb_table" "counter_table" {
+  name          = "counter"
   resource_group_name = data.azurerm_resource_group.devops.name
-  account_name        = azurerm_cosmosdb_account.cosmos.name
+  account_name  = azurerm_cosmosdb_account.cosmos.name
+  throughput    = 400
 }
 
 # -----------------------------
-# Service Plan (Linux)
+# Service Plan (Linux Consumption)
 # -----------------------------
 resource "azurerm_service_plan" "function_plan" {
   name                = "azure-be-plan"
-  location            = "Canada Central"
+  location            = data.azurerm_resource_group.devops.location
   resource_group_name = data.azurerm_resource_group.devops.name
   os_type             = "Linux"
-  sku_name            = "Y1"
-  worker_count        = 1
+  sku_name            = "Y1"  # Linux Consumption plan
 }
 
 # -----------------------------
@@ -75,7 +76,7 @@ resource "azurerm_service_plan" "function_plan" {
 # -----------------------------
 resource "azurerm_linux_function_app" "function_app" {
   name                       = "azure-be"
-  location                   = "Canada Central"
+  location                   = data.azurerm_resource_group.devops.location
   resource_group_name        = data.azurerm_resource_group.devops.name
   service_plan_id            = azurerm_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.funcsa.name
@@ -85,9 +86,9 @@ resource "azurerm_linux_function_app" "function_app" {
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "python"
     "AzureWebJobsStorage"      = azurerm_storage_account.funcsa.primary_connection_string
-    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_key
-    "TABLE_NAME"               = azurerm_cosmosdb_table.table.name
+    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.table_endpoint
+    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_master_key
+    "TABLE_NAME"               = azurerm_cosmosdb_table.counter_table.name
   }
 
   site_config {
