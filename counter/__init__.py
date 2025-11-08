90cf6b10250b1b8935526d5eba1a23b406174fbd
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 21:59:55 +0500

    Workflow Added

--- counter/__init__.py
+++ counter/__init__.py
@@ -9,33 +9,28 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.function_name(name="counter")
 @app.route(route="counter")  # URL path = /api/counter
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Get environment variables from Function App settings
-    endpoint = os.environ.get("COSMOS_TABLE_ENDPOINT")
+    # Use connection string from Terraform-managed environment variable
     connection_string = os.environ.get("COSMOS_TABLE_CONNECTION_STRING")
     table_name = os.environ.get("TABLE_NAME", "counter")
 
     partition_key = "counter"
     row_key = "visitors"
 
-    # Connect to the table using connection string
-    client = TableClient.from_connection_string(
-        conn_str=connection_string,
-        table_name=table_name
-    )
+    # Connect to the table
+    client = TableClient.from_connection_string(conn_str=connection_string, table_name=table_name)
 
     try:
-        # Try to get the existing entity
+        # Try to get the entity
         entity = client.get_entity(partition_key=partition_key, row_key=row_key)
         count = int(entity.get("count", 0)) + 1
         entity["count"] = count
         client.update_entity(entity, mode="Replace")
     except ResourceNotFoundError:
-        # If entity not found, create it
+        # If entity does not exist, create it
         entity = {"PartitionKey": partition_key, "RowKey": row_key, "count": 1}
         client.create_entity(entity)
         count = 1
 
-    # Return JSON response with current count
     return func.HttpResponse(
         body=json.dumps({"visitors": count}),
         mimetype="application/json",
--- main.tf
+++ main.tf
@@ -1,119 +1,34 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
-    }
-  }
-}
-
-# -----------------------------
-# Provider configuration
-# -----------------------------
-provider "azurerm" {
-  features {}
-
-  # If running in GitHub Actions with azure/login@v2,
-  # these will automatically be populated from AZURE_CREDENTIALS
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-}
-
-# -----------------------------
-# Variables for provider
-# -----------------------------
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-
-# -----------------------------
-# Existing Resource Group
-# -----------------------------
-data "azurerm_resource_group" "devops" {
-  name = "DevOps"
-}
-
-# -----------------------------
-# Storage Account for Function
-# -----------------------------
-resource "azurerm_storage_account" "funcsa" {
-  name                     = "azurebefuncsa"
-  resource_group_name      = data.azurerm_resource_group.devops.name
-  location                 = data.azurerm_resource_group.devops.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# -----------------------------
-# Cosmos DB Account (Table API)
-# -----------------------------
-resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "azure-be"
-  location            = "Canada Central"
-  resource_group_name = data.azurerm_resource_group.devops.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-
-  consistency_policy {
-    consistency_level = "Session"
-  }
-
-  geo_location {
-    location          = "Canada Central"
-    failover_priority = 0
-  }
-
-  capabilities {
-    name = "EnableTable"
-  }
-}
-
-# -----------------------------
-# Cosmos DB Table
-# -----------------------------
-resource "azurerm_cosmosdb_table" "table" {
-  name                = "counter"
-  resource_group_name = data.azurerm_resource_group.devops.name
-  account_name        = azurerm_cosmosdb_account.cosmos.name
-}
-
-# -----------------------------
-# Service Plan for Function App
-# -----------------------------
-resource "azurerm_service_plan" "function_plan" {
-  name                = "azure-be-plan"
-  location            = "Canada Central"
-  resource_group_name = data.azurerm_resource_group.devops.name
-  os_type             = "Linux"
-  sku_name            = "Y1"
-}
-
-# -----------------------------
-# Function App
-# -----------------------------
-resource "azurerm_linux_function_app" "function_app" {
-  name                       = "azure-be"
-  location                   = "Canada Central"
-  resource_group_name        = data.azurerm_resource_group.devops.name
-  service_plan_id            = azurerm_service_plan.function_plan.id
-  storage_account_name       = azurerm_storage_account.funcsa.name
-  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
-  functions_extension_version = "~4"
-
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"         = "python"
-    "AzureWebJobsStorage"              = azurerm_storage_account.funcsa.primary_connection_string
-    "TABLE_NAME"                       = azurerm_cosmosdb_table.table.name
-    "COSMOS_TABLE_ENDPOINT"            = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_CONNECTION_STRING"   = "DefaultEndpointsProtocol=https;AccountName=${azurerm_cosmosdb_account.cosmos.name};AccountKey=${azurerm_cosmosdb_account.cosmos.primary_key};TableEndpoint=${azurerm_cosmosdb_account.cosmos.endpoint};"
-  }
-
-  site_config {
-    application_stack {
-      python_version = "3.10"
-    }
-  }
-}
+azurerm_service_plan.function_plan: Creating...
+azurerm_cosmosdb_account.cosmos: Creating...
+azurerm_storage_account.funcsa: Creating...
+╷
+│ Error: a resource with the ID "/subscriptions/0bc7a085-204d-404f-a538-49b6b2ff9c1d/resourceGroups/DevOps/providers/Microsoft.Storage/storageAccounts/azurebefuncsa" already exists - to be managed via Terraform this resource needs to be imported into the State. Please see the resource documentation for "azurerm_storage_account" for more information
+│ 
+│   with azurerm_storage_account.funcsa,
+│   on main.tf line 42, in resource "azurerm_storage_account" "funcsa":
+│   42: resource "azurerm_storage_account" "funcsa" ***
+│ 
+╵
+╷
+│ Error: a resource with the ID "/subscriptions/0bc7a085-204d-404f-a538-49b6b2ff9c1d/resourceGroups/DevOps/providers/Microsoft.DocumentDB/databaseAccounts/azure-be" already exists - to be managed via Terraform this resource needs to be imported into the State. Please see the resource documentation for "azurerm_cosmosdb_account" for more information
+│ 
+│   with azurerm_cosmosdb_account.cosmos,
+│   on main.tf line 53, in resource "azurerm_cosmosdb_account" "cosmos":
+│   53: resource "azurerm_cosmosdb_account" "cosmos" ***
+│ 
+╵
+╷
+│ Error: a resource with the ID "/subscriptions/0bc7a085-204d-404f-a538-49b6b2ff9c1d/resourceGroups/DevOps/providers/Microsoft.Web/serverFarms/azure-be-plan" already exists - to be managed via Terraform this resource needs to be imported into the State. Please see the resource documentation for "azurerm_service_plan" for more information
+│ 
+│   with azurerm_service_plan.function_plan,
+│   on main.tf line 86, in resource "azurerm_service_plan" "function_plan":
+│   86: resource "azurerm_service_plan" "function_plan" ***
+│ 
+│ a resource with the ID
+│ "/subscriptions/0bc7a085-204d-404f-a538-49b6b2ff9c1d/resourceGroups/DevOps/providers/Microsoft.Web/serverFarms/azure-be-plan"
+│ already exists - to be managed via Terraform this resource needs to be
+│ imported into the State. Please see the resource documentation for
+│ "azurerm_service_plan" for more information
+╵
+Error: Terraform exited with code 1.
+Error: Process completed with exit code 1.
