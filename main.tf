bc6a78c369aa94ab33a47695098931c26551658a
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 23:17:32 +0500

    Workflow Added

--- main.tf
+++ main.tf
@@ -2,41 +2,34 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0"
+      version = ">=3.0"
     }
   }
-
-  required_version = ">= 1.5.0"
+  required_version = ">=1.3.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-#####################
 # Resource Group
-#####################
 resource "azurerm_resource_group" "rg" {
   name     = "DevOps"
   location = "East US"
 }
 
-#####################
-# Storage Account for Function App
-#####################
+# Storage Account
 resource "azurerm_storage_account" "funcsa" {
-  name                     = "azurefuncsa1234" # must be globally unique
+  name                     = "azurebefuncsa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-#####################
 # Cosmos DB Account
-#####################
 resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "azurebecosmos1234" # must be globally unique
+  name                = "azurebecosmos"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -46,55 +39,40 @@ resource "azurerm_cosmosdb_account" "cosmos" {
     consistency_level = "Session"
   }
 
-  capabilities {
-    name = "EnableTable"
-  }
-
   geo_location {
     location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
 }
 
-#####################
 # Cosmos DB Table
-#####################
 resource "azurerm_cosmosdb_table" "counter_table" {
   name                = "counter"
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.cosmos.name
   throughput          = 400
-
-  partition_key {
-    name = "PartitionKey"
-    type = "String"
-    kind = "Hash"
-  }
+  partition_key_path  = "/PartitionKey"
 }
 
-#####################
-# App Service Plan (Linux Function App)
-#####################
+# App Service Plan
 resource "azurerm_service_plan" "function_plan" {
-  name                = "function-plan"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "azure-be-plan"
   location            = azurerm_resource_group.rg.location
-
-  os_type  = "Linux"
-  sku_name = "Y1"  # Consumption Plan
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Y1"
+  os_type             = "Linux"
+  kind                = "FunctionApp"
 }
 
-#####################
 # Linux Function App
-#####################
 resource "azurerm_linux_function_app" "function_app" {
-  name                = "visitor-counter-func"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.function_plan.id
+  name                       = "azure-be-func"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.function_plan.id
   storage_account_name       = azurerm_storage_account.funcsa.name
   storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
-  version                   = "~4"
+  version                    = "~4"
   site_config {
     application_stack {
       python_version = "3.11"
@@ -102,10 +80,9 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 
   app_settings = {
-    "AzureWebJobsStorage"     = azurerm_storage_account.funcsa.primary_connection_string
     "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "COSMOS_TABLE_ENDPOINT"   = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_KEY"        = azurerm_cosmosdb_account.cosmos.primary_master_key
-    "TABLE_NAME"              = azurerm_cosmosdb_table.counter_table.name
+    "COSMOS_TABLE_NAME"        = azurerm_cosmosdb_table.counter_table.name
+    "COSMOS_TABLE_ACCOUNT"     = azurerm_cosmosdb_account.cosmos.name
+    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_master_key
   }
 }
