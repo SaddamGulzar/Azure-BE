cdd7f4126e939170e9508a7cb5d3e83ea4edeace
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 21:40:50 +0500

    Workflow Added

--- counter/__init__.py
+++ counter/__init__.py
@@ -1,35 +1,45 @@
 import azure.functions as func
-from azure.data.tables import TableClient
+from azure.data.tables import TableServiceClient
 from azure.core.exceptions import ResourceNotFoundError
 import os
 import json
+import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="counter")
-@app.route(route="counter")  # URL path = /api/counter
+@app.route(route="counter")
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    endpoint = os.environ.get("COSMOS_TABLE_ENDPOINT")
-    key = os.environ.get("COSMOS_TABLE_KEY")
-    table_name = os.environ.get("TABLE_NAME", "counter")
-
-    partition_key = "counter"
-    row_key = "visitors"
-
-    client = TableClient(endpoint=endpoint, table_name=table_name, credential=key)
-
     try:
-        entity = client.get_entity(partition_key=partition_key, row_key=row_key)
-        count = int(entity.get("count", 0)) + 1
-        entity["count"] = count
-        client.update_entity(entity, mode="Replace")
-    except ResourceNotFoundError:
-        entity = {"PartitionKey": partition_key, "RowKey": row_key, "count": 1}
-        client.create_entity(entity)
-        count = 1
+        connection_string = os.environ.get("COSMOS_TABLE_CONNECTION_STRING")
+        table_name = os.environ.get("TABLE_NAME", "counter")
 
-    return func.HttpResponse(
-        body=json.dumps({"visitors": count}),
-        mimetype="application/json",
-        status_code=200
-    )
+        service = TableServiceClient.from_connection_string(conn_str=connection_string)
+        client = service.get_table_client(table_name=table_name)
+
+        partition_key = "counter"
+        row_key = "visitors"
+
+        try:
+            entity = client.get_entity(partition_key=partition_key, row_key=row_key)
+            count = int(entity.get("count", 0)) + 1
+            entity["count"] = count
+            client.update_entity(entity, mode="Replace")
+        except ResourceNotFoundError:
+            entity = {"PartitionKey": partition_key, "RowKey": row_key, "count": 1}
+            client.create_entity(entity)
+            count = 1
+
+        return func.HttpResponse(
+            body=json.dumps({"visitors": count}),
+            mimetype="application/json",
+            status_code=200
+        )
+
+    except Exception as e:
+        logging.error(f"Error in counter function: {str(e)}")
+        return func.HttpResponse(
+            body=json.dumps({"error": str(e)}),
+            mimetype="application/json",
+            status_code=500
+        )
--- main.tf
+++ main.tf
@@ -11,9 +11,7 @@ provider "azurerm" {
   features {}
 }
 
-# -----------------------------
 # Existing Resource Group
-# -----------------------------
 data "azurerm_resource_group" "devops" {
   name = "DevOps"
 }
@@ -53,7 +51,7 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   }
 }
 
-# Database Table
+# Table for visitor counter
 resource "azurerm_cosmosdb_table" "table" {
   name                = "counter"
   resource_group_name = data.azurerm_resource_group.devops.name
@@ -69,32 +67,31 @@ resource "azurerm_service_plan" "function_plan" {
   resource_group_name = data.azurerm_resource_group.devops.name
   os_type             = "Linux"
   sku_name            = "Y1"
-  worker_count        = 1
 }
 
 # -----------------------------
-# Function App
+# Function App (Python)
 # -----------------------------
 resource "azurerm_linux_function_app" "function_app" {
-  name                        = "azure-be"
-  location                    = "Canada Central"
-  resource_group_name         = data.azurerm_resource_group.devops.name
-  service_plan_id             = azurerm_service_plan.function_plan.id
-  storage_account_name        = azurerm_storage_account.funcsa.name
-  storage_account_access_key  = azurerm_storage_account.funcsa.primary_access_key
+  name                       = "azure-be"
+  location                   = "Canada Central"
+  resource_group_name        = data.azurerm_resource_group.devops.name
+  service_plan_id            = azurerm_service_plan.function_plan.id
+  storage_account_name       = azurerm_storage_account.funcsa.name
+  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
   functions_extension_version = "~4"
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"      = "python"
+    "AzureWebJobsStorage"           = azurerm_storage_account.funcsa.primary_connection_string
+    "TABLE_NAME"                    = azurerm_cosmosdb_table.table.name
+    "COSMOS_TABLE_ENDPOINT"         = azurerm_cosmosdb_account.cosmos.endpoint
+    "COSMOS_TABLE_CONNECTION_STRING" = "DefaultEndpointsProtocol=https;AccountName=${azurerm_cosmosdb_account.cosmos.name};AccountKey=${azurerm_cosmosdb_account.cosmos.primary_key};TableEndpoint=${azurerm_cosmosdb_account.cosmos.endpoint};"
+  }
 
   site_config {
     application_stack {
-      python_version = "3.11"
+      python_version = "3.10"
     }
-  }
-
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "AzureWebJobsStorage"      = azurerm_storage_account.funcsa.primary_connection_string
-    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_key
-    "TABLE_NAME"               = azurerm_cosmosdb_table.table.name
   }
 }
