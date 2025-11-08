41f8b9a767cbeff32e5418f410fd76555439f3bd
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-08 23:46:10 +0500

    Workflow Added

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.70"
+      version = ">=3.0.0"
     }
   }
-  required_version = ">= 1.7"
 }
 
 provider "azurerm" {
@@ -20,23 +21,23 @@ resource "azurerm_resource_group" "rg" {
 
 # Storage Account for Function App
 resource "azurerm_storage_account" "funcsa" {
-  name                     = "azurefuncsa1234" # must be globally unique
+  name                     = "azurebefuncsa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Cosmos DB Account
+# Cosmos DB Account (SQL API)
 resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "azurebecosmos1234" # must be globally unique
+  name                = "azure-be"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
 
   consistency_policy {
-    consistency_level       = "Session"
+    consistency_level = "Session"
   }
 
   geo_location {
@@ -54,33 +55,34 @@ resource "azurerm_cosmosdb_sql_database" "counter_db" {
 
 # Cosmos DB SQL Container
 resource "azurerm_cosmosdb_sql_container" "counter_container" {
-  name                = "counter"
+  name                = "countercontainer"
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.cosmos.name
   database_name       = azurerm_cosmosdb_sql_database.counter_db.name
 
-  partition_key_paths = ["/PartitionKey"]  # Must be a list
-  throughput          = 400
+  partition_key_path = "/PartitionKey"
+  throughput         = 400
 }
 
-# App Service Plan for Function App
+# App Service Plan
 resource "azurerm_service_plan" "function_plan" {
-  name                = "azure-function-plan"
+  name                = "azure-be-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Y1"  # Consumption plan
-  os_type             = "Linux"
+
+  sku_name = "Y1" # Consumption plan
+  os_type  = "Linux"
 }
 
 # Linux Function App
 resource "azurerm_linux_function_app" "function_app" {
-  name                = "azure-function-app1234"  # must be globally unique
+  name                = "my-function-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.function_plan.id
-  storage_account_name = azurerm_storage_account.funcsa.name
+
+  storage_account_name       = azurerm_storage_account.funcsa.name
   storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
-  version             = "~4"
 
   site_config {
     application_stack {
@@ -89,7 +91,7 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 
   app_settings = {
-    "COSMOS_DB_ACCOUNT" = azurerm_cosmosdb_account.cosmos.name
-    "COSMOS_DB_KEY"     = azurerm_cosmosdb_account.cosmos.primary_master_key
+    "COSMOS_DB_KEY"  = azurerm_cosmosdb_account.cosmos.primary_key
+    "COSMOS_DB_NAME" = azurerm_cosmosdb_account.cosmos.name
   }
 }
