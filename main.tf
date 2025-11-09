15fdf21270382d7f15e46de0321b0f37946dfb75
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-09 12:30:02 +0500

    final update

--- main.tf
+++ main.tf
@@ -9,23 +9,17 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
 
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-
+# -----------------------------
+# Existing Resource Group
+# -----------------------------
 data "azurerm_resource_group" "devops" {
   name = "DevOps"
 }
 
 # -----------------------------
-# Storage Account
+# Storage Account for Function
 # -----------------------------
 resource "azurerm_storage_account" "funcsa" {
   name                     = "azurebefuncsa"
@@ -33,20 +27,12 @@ resource "azurerm_storage_account" "funcsa" {
   location                 = data.azurerm_resource_group.devops.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  allow_nested_items_to_be_public = false
-  min_tls_version                 = "TLS1_2"
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = all
-  }
 }
 
 # -----------------------------
-# Cosmos DB Account
+# Cosmos DB (Table API)
 # -----------------------------
-resource "azurerm_cosmosdb_account" "db" {
+resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "azure-be"
   location            = "Canada Central"
   resource_group_name = data.azurerm_resource_group.devops.name
@@ -65,63 +51,50 @@ resource "azurerm_cosmosdb_account" "db" {
   capabilities {
     name = "EnableTable"
   }
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = all
-  }
 }
 
-# -----------------------------
-# Cosmos DB Table
-# -----------------------------
+# Database Table
 resource "azurerm_cosmosdb_table" "table" {
   name                = "counter"
   resource_group_name = data.azurerm_resource_group.devops.name
-  account_name        = azurerm_cosmosdb_account.db.name
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = all
-  }
+  account_name        = azurerm_cosmosdb_account.cosmos.name
 }
 
 # -----------------------------
-# Use Existing Service Plan (avoids Dynamic VM quota issues)
+# Service Plan (Linux)
 # -----------------------------
-data "azurerm_service_plan" "existing" {
-  name                = "existing-plan" # <-- Replace with your pre-created plan
+resource "azurerm_service_plan" "function_plan" {
+  name                = "azure-be-plan"
+  location            = "Canada Central"
   resource_group_name = data.azurerm_resource_group.devops.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+  worker_count        = 1
 }
 
 # -----------------------------
-# Linux Function App
+# Function App
 # -----------------------------
 resource "azurerm_linux_function_app" "function_app" {
-  name                       = "azure-be"
-  location                   = data.azurerm_resource_group.devops.location
-  resource_group_name        = data.azurerm_resource_group.devops.name
-  service_plan_id            = data.azurerm_service_plan.existing.id
-  storage_account_name       = azurerm_storage_account.funcsa.name
-  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
+  name                        = "azure-be"
+  location                    = "Canada Central"
+  resource_group_name         = data.azurerm_resource_group.devops.name
+  service_plan_id             = azurerm_service_plan.function_plan.id
+  storage_account_name        = azurerm_storage_account.funcsa.name
+  storage_account_access_key  = azurerm_storage_account.funcsa.primary_access_key
   functions_extension_version = "~4"
 
   site_config {
     application_stack {
-      python_version = "3.10"
+      python_version = "3.11"
     }
   }
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"       = "python"
-    "AzureWebJobsStorage"            = azurerm_storage_account.funcsa.primary_connection_string
-    "TABLE_NAME"                     = azurerm_cosmosdb_table.table.name
-    "COSMOS_TABLE_ENDPOINT"          = azurerm_cosmosdb_account.db.endpoint
-    "COSMOS_TABLE_CONNECTION_STRING" = "DefaultEndpointsProtocol=https;AccountName=${azurerm_cosmosdb_account.db.name};AccountKey=${azurerm_cosmosdb_account.db.primary_key};TableEndpoint=${azurerm_cosmosdb_account.db.endpoint};"
-  }
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = all
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "AzureWebJobsStorage"      = azurerm_storage_account.funcsa.primary_connection_string
+    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.cosmos.endpoint
+    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.cosmos.primary_key
+    "TABLE_NAME"               = azurerm_cosmosdb_table.table.name
   }
 }
