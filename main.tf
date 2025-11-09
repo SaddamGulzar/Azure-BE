029c10557f3bff586fc9a81a33c93f4b20a49930
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-09 12:20:29 +0500

    final Update

--- main.tf
+++ main.tf
@@ -7,27 +7,25 @@ terraform {
   }
 }
 
-# -----------------------------
-# Provider configuration
-# -----------------------------
 provider "azurerm" {
   features {}
-
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
 }
 
-# -----------------------------
-# Existing Resource Group
-# -----------------------------
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 data "azurerm_resource_group" "devops" {
   name = "DevOps"
 }
 
 # -----------------------------
-# Storage Account for Function App
+# Storage Account
 # -----------------------------
 resource "azurerm_storage_account" "funcsa" {
   name                     = "azurebefuncsa"
@@ -38,10 +36,15 @@ resource "azurerm_storage_account" "funcsa" {
 
   allow_nested_items_to_be_public = false
   min_tls_version                 = "TLS1_2"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
 }
 
 # -----------------------------
-# Cosmos DB Account (Table API)
+# Cosmos DB Account
 # -----------------------------
 resource "azurerm_cosmosdb_account" "db" {
   name                = "azure-be"
@@ -62,6 +65,11 @@ resource "azurerm_cosmosdb_account" "db" {
   capabilities {
     name = "EnableTable"
   }
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
 }
 
 # -----------------------------
@@ -71,29 +79,31 @@ resource "azurerm_cosmosdb_table" "table" {
   name                = "counter"
   resource_group_name = data.azurerm_resource_group.devops.name
   account_name        = azurerm_cosmosdb_account.db.name
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
 }
 
 # -----------------------------
-# Service Plan for Function App
+# Use Existing Service Plan (avoids Dynamic VM quota issues)
 # -----------------------------
-resource "azurerm_service_plan" "function_plan" {
-  name                = "azure-be-plan"
-  location            = data.azurerm_resource_group.devops.location
+data "azurerm_service_plan" "existing" {
+  name                = "existing-plan" # <-- Replace with your pre-created plan
   resource_group_name = data.azurerm_resource_group.devops.name
-  os_type             = "Linux"
-  sku_name            = "Y1" # Consumption Plan
 }
 
 # -----------------------------
 # Linux Function App
 # -----------------------------
 resource "azurerm_linux_function_app" "function_app" {
-  name                        = "azure-be"
-  location                    = data.azurerm_resource_group.devops.location
-  resource_group_name         = data.azurerm_resource_group.devops.name
-  service_plan_id             = azurerm_service_plan.function_plan.id
-  storage_account_name        = azurerm_storage_account.funcsa.name
-  storage_account_access_key  = azurerm_storage_account.funcsa.primary_access_key
+  name                       = "azure-be"
+  location                   = data.azurerm_resource_group.devops.location
+  resource_group_name        = data.azurerm_resource_group.devops.name
+  service_plan_id            = data.azurerm_service_plan.existing.id
+  storage_account_name       = azurerm_storage_account.funcsa.name
+  storage_account_access_key = azurerm_storage_account.funcsa.primary_access_key
   functions_extension_version = "~4"
 
   site_config {
@@ -108,5 +118,10 @@ resource "azurerm_linux_function_app" "function_app" {
     "TABLE_NAME"                     = azurerm_cosmosdb_table.table.name
     "COSMOS_TABLE_ENDPOINT"          = azurerm_cosmosdb_account.db.endpoint
     "COSMOS_TABLE_CONNECTION_STRING" = "DefaultEndpointsProtocol=https;AccountName=${azurerm_cosmosdb_account.db.name};AccountKey=${azurerm_cosmosdb_account.db.primary_key};TableEndpoint=${azurerm_cosmosdb_account.db.endpoint};"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
   }
 }
