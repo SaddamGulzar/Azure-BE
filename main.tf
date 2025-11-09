90f06dc303b4787266415dff83e8a592c5b7da25
Muhammad Saddam Gulzar <saddam_gulzar@live.com>
2025-11-09 11:04:25 +0500

    final Update

--- main.tf
+++ main.tf
@@ -2,94 +2,109 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
+      version = "~>3.100"
     }
   }
+
+  required_version = ">= 1.5.0"
+
+  backend "local" {} # Optional: change to azurerm backend if you use remote state
 }
 
-# -----------------------------
-# Provider configuration
-# -----------------------------
 provider "azurerm" {
   features {}
-
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
 }
 
-# -----------------------------
-# Variables for provider
-# -----------------------------
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-
-# -----------------------------
-# Existing Resource Group
-# -----------------------------
-data "azurerm_resource_group" "devops" {
-  name = "DevOps"
+# ---------------------
+# Resource Group
+# ---------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "visitor-rg"
+  location = "East US"
 }
 
-# -----------------------------
-# EXISTING Storage Account
-# -----------------------------
-data "azurerm_storage_account" "funcsa" {
-  name                = "azurebefuncsa"
-  resource_group_name = data.azurerm_resource_group.devops.name
+# ---------------------
+# Storage Account (for Function App)
+# ---------------------
+resource "azurerm_storage_account" "storage" {
+  name                     = "visitorstorage${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-# -----------------------------
-# EXISTING Cosmos DB Account
-# -----------------------------
-data "azurerm_cosmosdb_account" "cosmos" {
-  name                = "azure-be"
-  resource_group_name = data.azurerm_resource_group.devops.name
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
-# -----------------------------
-# EXISTING Cosmos DB Table
-# -----------------------------
-data "azurerm_cosmosdb_table" "table" {
-  name                = "counter"
-  resource_group_name = data.azurerm_resource_group.devops.name
-  account_name        = data.azurerm_cosmosdb_account.cosmos.name
-}
+# ---------------------
+# Cosmos DB Account
+# ---------------------
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "visitorsdb${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
 
-# -----------------------------
-# EXISTING Service Plan
-# -----------------------------
-data "azurerm_service_plan" "function_plan" {
-  name                = "azure-be-plan"
-  resource_group_name = data.azurerm_resource_group.devops.name
-}
-
-# -----------------------------
-# Function App
-# -----------------------------
-resource "azurerm_linux_function_app" "function_app" {
-  name                        = "azure-be"
-  location                    = "Canada Central"
-  resource_group_name         = data.azurerm_resource_group.devops.name
-  service_plan_id             = data.azurerm_service_plan.function_plan.id
-  storage_account_name        = data.azurerm_storage_account.funcsa.name
-  storage_account_access_key  = data.azurerm_storage_account.funcsa.primary_access_key
-  functions_extension_version = "~4"
-
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"         = "python"
-    "AzureWebJobsStorage"              = data.azurerm_storage_account.funcsa.primary_connection_string
-    "TABLE_NAME"                       = data.azurerm_cosmosdb_table.table.name
-    "COSMOS_TABLE_ENDPOINT"            = data.azurerm_cosmosdb_account.cosmos.endpoint
-    "COSMOS_TABLE_CONNECTION_STRING"   = "DefaultEndpointsProtocol=https;AccountName=${data.azurerm_cosmosdb_account.cosmos.name};AccountKey=${data.azurerm_cosmosdb_account.cosmos.primary_key};TableEndpoint=${data.azurerm_cosmosdb_account.cosmos.endpoint};"
+  consistency_policy {
+    consistency_level = "Session"
   }
+
+  capabilities {
+    name = "EnableTable"
+  }
+}
+
+# ---------------------
+# Cosmos DB Table
+# ---------------------
+resource "azurerm_cosmosdb_table" "table" {
+  name                = "VisitorCount"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.db.name
+}
+
+# ---------------------
+# App Service Plan
+# ---------------------
+resource "azurerm_service_plan" "plan" {
+  name                = "visitor-func-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+# ---------------------
+# Linux Function App
+# ---------------------
+resource "azurerm_linux_function_app" "function_app" {
+  name                = "visitor-function-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
 
   site_config {
     application_stack {
       python_version = "3.10"
     }
+  }
+
+  app_settings = {
+    "AzureWebJobsStorage" = azurerm_storage_account.storage.primary_connection_string
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "COSMOS_TABLE_ENDPOINT"    = azurerm_cosmosdb_account.db.table_endpoint
+    "COSMOS_TABLE_KEY"         = azurerm_cosmosdb_account.db.primary_key
+    "COSMOS_TABLE_NAME"        = azurerm_cosmosdb_table.table.name
   }
 }
